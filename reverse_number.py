6633ed8fcf38b10054440bdf405d54062152dfaf
Sonali Patil <118295011+Sonali2314@users.noreply.github.com>
2024-09-29 21:57:26 +0530

    reverse_number.py

---
+++ reverse_number.py
@@ -0,0 +1,21 @@
+def reverse(x: int) -> int:
+    result = 0
+    sign = 1 if x >= 0 else -1
+    x = abs(x)
+
+    while x != 0:
+        digit = x % 10
+        result = result * 10 + digit
+        x //= 10
+
+    result *= sign
+    if result < -2**31 or result > 2**31 - 1:
+        return 0
+    else:
+        return result
+
+x=int(input('Enter the number : '))
+n=reverse(x)
+print('Reverse of ',x,' is ',n)
+        
+        
